081fd278cb6c329adcb030f4d617daf5784b13b5
markmcgookin <mark.mcgookin@gmail.com>
2019-07-18 23:36:29 +0100

    added some navigation functionality

---
+++ src/js/custom.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function() {
+    var $ = jQuery;
+
+    $(document).keydown(function(e) {
+        var ahref = document.getElementById('gohome');
+        ahref.click();
+    });
+});
