85a195f177b2e97e0f390d6dc99ddfb24e8828e2
markmcgookin <mark.mcgookin@gmail.com>
2019-08-06 22:55:56 +0100

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,27 @@
+#Remove the old folder if it exists
+rm -r -f /opt/status-monitor
+
+#Create a clean directory
+mkdir -p /opt/status-monitor
+
+#Download the latest version
+wget http://jenkins.local:8081/builds/status-monitor/status-monitor-latest.tar.gz
+
+#Extract to the install directory
+tar zxf status-monitor-latest.tar.gz -C /opt/status-monitor
+
+#Cleanup the download
+rm -f status-monitor-latest.tar.gz
+
+#Setup docker stuff
 docker pull dockerregistry.local:5000/markmcgookin/serial-port-monitor:latest
 docker pull dockerregistry.local:5000/markmcgookin/contact-ui:latest
 
-docker run --name serial-port -d  -e SERIAL_PORT="/dev/ttyACM0" --device=/dev/ttyACM0 --restart unless-stopped dockerregistry.local:5000/markmcgookin/serial-port-monitor
-docker run --name contact-ui --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --restart unless-stopped dockerregistry.local:5000/markmcgookin/contact-ui
+docker stop contact-ui
+docker stop serial-port
+
+docker rm contact-ui
+docker rm serial-port
+
+docker create --name serial-port -e SERIAL_PORT="/dev/ttyACM0" --device=/dev/ttyACM0 dockerregistry.local:5000/markmcgookin/serial-port-monitor
+docker create --name contact-ui --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw"  dockerregistry.local:5000/markmcgookin/contact-ui
--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 docker start serial-port
 docker start contact-ui
+
+STATUS_OUTPUT=/output/info.dat
+nohup dotnet /opt/status-monitor/out/status-monitor.dll > /dev/null 2>&1 &
