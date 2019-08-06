8c0e07fd48b697af3bcb7311cbfd590a25b5a3f5
markmcgookin <mark.mcgookin@gmail.com>
2019-08-06 19:58:12 +0100

    removed javascript project

--- javascript/README.md
+++
@@ -1,2 +0,0 @@
-# contact-ui
-A small electron application providing a touch screen GUI for functions running on the RPI
--- javascript/src/js/custom.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-    var $ = jQuery;
-
-    $(document).keydown(function(e) {
-        var ahref = document.getElementById('gohome');
-        ahref.click();
-    });
-});
